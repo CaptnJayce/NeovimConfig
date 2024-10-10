a4081e1aa1e7e6e92b838cd8d7b54efbd7f50c01
CaptnJayce <captn.dev@protonmail.com>
2024-10-10 23:01:31 +0100

    lspconfig

--- lua/nvim-settings.lua
+++ lua/nvim-settings.lua
@@ -1,8 +1,9 @@
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")    
+vim.cmd("set shiftwidth=2")
 vim.wo.relativenumber = true
 vim.g.mapleader = " "
 
-
+vim.keymap.set('n', '<leader>K', vim.lsp.buf.hover, {})
+vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})

99f958f0f396c2d4d774713fd628249b17386b3b
CaptnJayce <captn.dev@protonmail.com>
2024-10-11 01:07:48 +0100

    transparency

--- lua/plugins/rosepine.lua
+++ lua/plugins/rosepine.lua
@@ -1,9 +1,19 @@
-return { 
-  "rose-pine/neovim", 
-  name = "rose-pine", 
+return {
+	"rose-pine/neovim",
+	name = "rose-pine",
 
-  config = function()
-    vim.cmd.colorscheme "rose-pine"
-  end
+	config = function()
+		require("rose-pine").setup({
+			styles = {
+				bold = true,
+				italic = true,
+				transparency = true,
+			},
+
+			before_highlight = function()
+			end,
+		})
+
+		vim.cmd.colorscheme("rose-pine")
+	end,
 }
-

ffba86d98b0f0b65285d281ae8f6b08895063385
Jayce <captn.dev@protonmail.com>
2025-02-14 16:37:35 +0000

    lsp correction

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -10,7 +10,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "clangd", "python-lsp-server" },
+        ensure_installed = { "lua_ls", "clangd", "pylsp" },
       })
     end
   },
